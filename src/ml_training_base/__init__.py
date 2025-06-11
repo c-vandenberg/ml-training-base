a895810cab073ff90516357397d3d20e53c60cb1
Chris van den Berg <cvandenberg1105@googlemail.com>
2025-06-11 01:13:31 +0100

    Remove src from public API exposire imports

--- src/ml_training_base/__init__.py
+++ src/ml_training_base/__init__.py
@@ -1,21 +1,21 @@
 """
 ml-training-base: A Python package providing base classes and utilities for machine learning projects
 """
-from src.ml_training_base.utils.logging.logging_utils import configure_logger
+from ml_training_base.utils.logging.logging_utils import configure_logger
 
-from src.ml_training_base.supervised.environments.base_training_environments import (
+from ml_training_base.supervised.environments.base_training_environments import (
     BaseTrainingEnvironment,
     KerasTrainingEnvironment,
     PyTorchTrainingEnvironment
 )
 
-from src.ml_training_base.supervised.trainers.base_supervised_trainers import (
+from ml_training_base.supervised.trainers.base_supervised_trainers import (
     BaseSupervisedTrainer,
     BaseKerasSupervisedTrainer,
     BasePyTorchSupervisedTrainer
 )
 
-from src.ml_training_base.supervised.utils.data.base_supervised_data_loader import BaseSupervisedDataLoader
+from ml_training_base.supervised.utils.data.base_supervised_data_loader import BaseSupervisedDataLoader
 
 __all__ = [
     # Public Utility Functions
